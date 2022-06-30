be331d0d5de8aa4aac28f73f09ca4dbd8b66c9e5
v.rakovskiy <v.rakovskiy89@mail.ru>
2022-06-30 16:49:07 +0300

    add teamplate

--- local/templates/very-neat/header.php
+++ local/templates/very-neat/header.php
@@ -1,1 +1,11 @@
 <?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+$APPLICATION->SetTitle("very neat");
+$APPLICATION->ShowHead();
+use Bitrix\Main\Page\Asset;
+
+$APPLICATION->ShowPanel();
+Asset::getInstance()->addCss(SITE_TEMPLATE_PATH."/css/css.css");
+Asset::getInstance()->addJs(SITE_TEMPLATE_PATH."/js/js.js");
+Asset::getInstance()->addString("<meta>");
+Asset::getInstance()->addString("<link>");
+CJSCore::Init("jquery");
